0e36789aaa98cb7f0b8e55a3cda503f503eadea4
Jackson Robbins <jackrobbins1@gmail.com>
2019-04-05 09:56:40 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,14 @@
+require "pry"
+
+class Author
+
+  attr_accessor :name
+
+  # def initialize(name)
+  #   @name = name
+  # end
+
+end
+
+#binding.pry
+"sup"
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+require "pry"
+
+class Song
+
+  attr_accessor :title, :artist
+
+  # def initialize(name)
+  #   @name = name
+  # end
+
+end
+
+#binding.pry
+"sup"
